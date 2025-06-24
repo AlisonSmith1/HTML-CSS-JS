521f8849ec1b6058d956ee70b8de6519a4b09208
Lana <kainingyang22@gmail.com>
2025-06-24 15:38:38 +0800

    把pool連上railway

--- server/db.js
+++ server/db.js
@@ -2,14 +2,6 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// const pool = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "shop",
-//   password: "1234",
-//   port: 5432,
-// });
-
 // 資料庫連線設定（Render 上會自動設定 DATABASE_URL）
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
