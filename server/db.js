5808394c48f1ad28f154ee6f6212311f5ac56928
Lana <kainingyang22@gmail.com>
2025-06-27 10:55:24 +0800

    fix upload

--- server/db.js
+++ server/db.js
@@ -3,18 +3,18 @@ const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 
 // 資料庫連線設定（Render 上會自動設定 DATABASE_URL）
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: { rejectUnauthorized: false },
-// });
-
 const pool = new Pool({
-  user: "postgres", // 請確認你 PostgreSQL 使用者名稱
-  host: "localhost", // 不要帶 http:// 和 port
-  database: "shop",
-  password: "1234",
-  port: 5432,
-  ssl: false, // 本機可先關閉 ssl
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
+
+// const pool = new Pool({
+//   user: "postgres", // 請確認你 PostgreSQL 使用者名稱
+//   host: "localhost", // 不要帶 http:// 和 port
+//   database: "shop",
+//   password: "1234",
+//   port: 5432,
+//   ssl: false, // 本機可先關閉 ssl
+// });
 
 module.exports = pool;
