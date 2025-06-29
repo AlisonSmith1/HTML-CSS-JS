b710450c111b5d10169a0ced69c32115fc2d6edb
Lana <kainingyang22@gmail.com>
2025-06-29 22:23:23 +0800

    env

--- client/addNewScript.js
+++ client/addNewScript.js
@@ -97,7 +97,7 @@ async function setupImageUploadHandler() {
 
         const result = await res.json();
         console.log("上傳成功：", result);
-        imageUrl = result.secure_url;
+        imageUrl = result;
 
         console.log(imageUrl);
         document.getElementById("image_url").value = imageUrl;
